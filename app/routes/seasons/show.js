f17440cbbeb3dd5bb80ab251e505596322578bfc
Nathan <Burgess@Nathans-MacBook-Pro.local>
2017-04-08 15:49:00 -0400

    Got some season data for standings

--- app/routes/seasons/show.js
+++ app/routes/seasons/show.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params){
-    return params;
+    
+    return $.getJSON("http://ergast.com/api/f1/2008/driverStandings.json").then(function(response){
+      return response.MRData.StandingsTable.StandingsLists[0].DriverStandings
+    });
   }
 });
