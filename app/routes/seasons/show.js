187162c73bd4f5974c1ba0793651c2abbba7fd29
Nathan <Burgess@Nathans-MacBook-Pro.local>
2017-04-08 16:14:20 -0400

    Dynamic year for seasons

--- app/routes/seasons/show.js
+++ app/routes/seasons/show.js
@@ -2,8 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params){
-    
-    return $.getJSON("http://ergast.com/api/f1/2008/driverStandings.json").then(function(response){
+    return $.getJSON("http://ergast.com/api/f1/" + params.year + "/driverStandings.json").then(function(response){
       return response.MRData.StandingsTable.StandingsLists[0].DriverStandings
     });
   }
